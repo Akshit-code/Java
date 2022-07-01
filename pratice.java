27ee541dbb064a0071cef1371dcead15432d0402
Akshit Mandani <akshit.m@somaiya.edu>
2022-07-01 07:27:02 +0530

    Created few Basic Arrays Methods

--- pratice.java
+++ pratice.java
@@ -1,28 +1,101 @@
 import java.util.*;
 
-class pratice {
-    public static void main(String[] args) {
-      ArrayList<Integer> arr1 = new ArrayList<>();
-      ArrayList<Integer> arr2 = new ArrayList<>();
-      Scanner sc = new Scanner(System.in);
-      System.out.println("Enter the size of Array");
-      int size = sc.nextInt();
+public class pratice {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        
 
-      for (int i=0; i<size; i++) {
-        int value = sc.nextInt();
-        arr1.add(value);
-      }
+        for (int i=0; i<size; i++) {
+            arr[i] = sc.nextInt();
+        }
 
-      System.out.println(arr1);
-      System.out.println(arr1.size());
-      arr2.addAll(arr1);
-      System.out.println(arr2);
-      arr2.addAll(2,arr1);
-      System.out.println(arr2);
-      Collections.sort(arr2);
-      System.out.println(arr2);
-      System.out.println(Collections.binarySearch(arr2, 2));
-    
-      sc.close();
+        System.out.println(Arrays.toString(arr));
+        leftrotate(arr);
+        System.out.println(Arrays.toString(arr));
+        rightrotate(arr);
+        System.out.println(Arrays.toString(arr));
+        System.out.println(" ");
+        reverse_Order(arr);
+        System.out.println(" ");
+        even_index_elements(arr);
+        System.out.println(" ");
+        odd_index_elements(arr);
+        System.out.println(" ");
+        largest_elements(arr);
+        System.out.println(" ");
+        smallest_elements(arr);
+        System.out.println(" ");
+
+        sc.close();
     }
-  }
+
+    static int[] leftrotate(int arr[]) {
+        int last = arr[0];
+        for (int i=0; i<arr.length-1; i++) {
+            arr[i] = arr[i +1];
+        }
+        arr[arr.length-1]= last;
+        return arr;
+    }
+
+    static int[] rightrotate(int arr[]) {
+        int first = arr[arr.length-1];
+        for (int i=arr.length-1; i>0; i--) {
+            arr[i] = arr[i-1];
+        }
+        arr[0] = first;
+        return arr;
+    }
+
+    static void reverse_Order(int arr[]) {
+
+        for (int i=arr.length-1; i>=0; i-- ) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void even_index_elements(int arr[]) {
+
+        for (int i=0; i<arr.length; i= i+2 ) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void odd_index_elements(int arr[]) {
+
+        for (int i=1; i<arr.length; i= i+2 ) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void largest_elements(int arr[]) {
+        int max = arr[0];
+        for (int i=1; i<arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i]; 
+            }  
+        }
+        System.out.println(max);
+    }
+
+    static void smallest_elements(int arr[]) {
+        int min = arr[0];
+        for (int i=1; i<arr.length; i++) {
+            if (arr[i] < max) {
+                min = arr[i]; 
+            }  
+        }
+        System.out.println(min);
+    }
+
+    static void descending_oder(int arr[]) {
+        int count = 0;
+
+        for (int i=0; i<arr.length; i++) {
+            
+        }
+    }
+}
